598fd573ab84750752f46d3e02ecf3435b67e6dc
Tejas <tejaskatke900a@gmail.com>
2022-11-25 22:09:51 +0530

    implemented Menu option with logout button..

--- app/src/main/java/com/example/twitterclone/Feed.java
+++ app/src/main/java/com/example/twitterclone/Feed.java
@@ -1,14 +1,39 @@
 package com.example.twitterclone;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.parse.ParseUser;
 
-public class Feed extends AppCompatActivity {
+public class Feed extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout){
+            ParseUser.getCurrentUser().logOut();
+            finish();
+            Intent intent = new Intent(Feed.this, LogIn.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
