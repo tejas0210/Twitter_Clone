dcaf9e14e56aaf1e00eb5b8fb46da809d44844fa
Tejas <tejaskatke900a@gmail.com>
2022-11-26 12:25:46 +0530

    Implemented Following Unfollowing feature

--- app/src/main/java/com/example/twitterclone/Feed.java
+++ app/src/main/java/com/example/twitterclone/Feed.java
@@ -6,7 +6,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,11 +22,12 @@ import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Feed extends AppCompatActivity {
+public class Feed extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ListView listView;
     private ArrayList<String> tUsers;
@@ -36,7 +40,9 @@ public class Feed extends AppCompatActivity {
 
         listView = findViewById(R.id.listView);
         tUsers = new ArrayList<>();
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,tUsers);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked,tUsers);
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+        listView.setOnItemClickListener(this::onItemClick);
 
         TextView txtLoadingUsers = findViewById(R.id.txtLoadingUsers);
 
@@ -53,6 +59,15 @@ public class Feed extends AppCompatActivity {
                         listView.setAdapter(arrayAdapter);
                         txtLoadingUsers.animate().alpha(0).setDuration(700);
                         txtLoadingUsers.setVisibility(View.VISIBLE);
+
+                        for(String user: tUsers){
+                            if(ParseUser.getCurrentUser().getList("Following")!=null){
+                                if(ParseUser.getCurrentUser().getList("Following").contains(user)){
+                                    listView.setItemChecked(tUsers.indexOf(user),true);
+                                }
+
+                            }
+                        }
                     }
                 }
             });
@@ -87,5 +102,33 @@ public class Feed extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        CheckedTextView checkedTextView = (CheckedTextView) view;
+
+        if(checkedTextView.isChecked()){
+            Toast.makeText(Feed.this,tUsers.get(position) +" is now Followed", Toast.LENGTH_SHORT).show();
+            ParseUser.getCurrentUser().add("Following",tUsers.get(position));
+        }
+        else{
+            Toast.makeText(Feed.this,tUsers.get(position) +" is now Unfollowed", Toast.LENGTH_SHORT).show();
+            ParseUser.getCurrentUser().getList("Following").remove(tUsers.get(position));
+            List currentUserFollowing = ParseUser.getCurrentUser().getList("Following");
+            ParseUser.getCurrentUser().remove("Following");
+            ParseUser.getCurrentUser().put("Following",currentUserFollowing);
+
+
+
+        }
+
+        ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e==null){
+                    Toast.makeText(Feed.this,"Saved", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
 }
